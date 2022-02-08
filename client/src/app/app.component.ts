3f2b332ebb6f14f4428e7e7de3179f532c58ab56
Vikas Sharma <contactvikas24@gmail.com>
2022-02-07 22:34:06 -0600

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {
+    setTheme('bs4');
+  }
+
+  ngOnInit() {
+      this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users')
+      .subscribe(response => {
+        this.users = response;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+
+}
