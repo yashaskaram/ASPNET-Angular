ed5465c037709bfabbaf83ea722262f4fa264283
Vikas Sharma <contactvikas24@gmail.com>
2022-02-26 03:53:33 -0600

    End of Section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.OpenApi.Models;
 using API.Extensions;
+using API.Middleware;
 
 namespace API
 {
@@ -29,13 +30,8 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
-
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,10 +23,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
       this.router.navigateByUrl('/members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    })
+    });
   }
 
   logout() {
